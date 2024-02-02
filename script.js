36eda36de88c39b52b9a9d82f79aacf6aab59960
ChanHW24 <chanhongwei24@gmail.com>
2024-02-03 03:46:10 +0800

    update 2.0.1 (change to password type)

--- script.js
+++ script.js
@@ -52,42 +52,4 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         })
     });
-
-    /*
-    function getContacts(limit = 10, all = true) {
-
-        // Create AJAX setting
-        let settings = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "x-apikey": APIKEY,
-                "Cache-Control": "no-cache"
-            },
-        }
-
-        // Make our AJAX calls
-        fetch("https://rollinggearstore-6adf.restdb.io/rest/login", settings)
-            .then(response => response.json())
-            .then(response => {
-                let content = "";
-
-                for (var i = 0; i < response.length && i < limit; i++) {
-                    content = `${content}<tr id='${response[i]._id}'>
-                        <td>${response[i].email}</td>
-                        <td>${response[i].text}</td>
-                        <td>
-                            <a href='#update-contact-container' class='update' data-id='${response[i]._id}' data-studentid='${response[i].Student_ID}' data-name='${response[i].Name}' data-email='${response[i].Student_Email}' data-studentclass='${response[i].Student_Class}' data-studentcourse='${response[i].Student_Course}' data-studentmentor='${response[i].Student_Mentor}'>Update</a>
-                            <a href='#' class='delete' data-id='${response[i]._id}'>Delete</a>
-                        </td>`;
-                }
-
-                // Update HTML content
-                document.getElementById("contact-list").getElementsByTagName("tbody")[0].innerHTML = content;
-                document.getElementById("total-contacts").innerHTML = response.length;
-            });
-
-    }
-    */
-
-});
+})
